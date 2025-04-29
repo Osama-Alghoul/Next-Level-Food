992503d7eaa6986a76b4474e1161df67102ecbd4
OsssGhoul <osamalghoul2@gmail.com>
2025-04-29 16:25:14 +0300

    not found and error handling

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,10 +1,15 @@
 import Link from 'next/link';
 import classes from './page.module.css';
+import styles from './loading.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import { getMeals } from '@/lib/meals';
+import { Suspense } from 'react';
 
-export default async function Meals() {
+async function Meals() {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />
+}
+export default function MealsPage() {
   return (
     <>
       <header className={classes.header}>
@@ -17,7 +22,9 @@ export default async function Meals() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<p className={styles.loading}>Featching meals...</p>}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
